21cd7b0b0f565ab4e7eeb22d3b7042e92f00fa41
Gustavo Navarro <genr1818@gmail.com>
2022-03-15 14:39:40 -0600

    PostgreSQL Database Configuration.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,9 +90,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'userlab',
+        'USER': 'postgres',
+        'PASSWORD': 'Password1_',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
