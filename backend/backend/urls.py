0c5842fde5a694f7023ebe8ff2d4c037b452ebec
Gustavo Navarro <genr1818@gmail.com>
2022-06-28 17:39:34 -0500

    API configuration and routes moved to its own config/api.py and urls.py  files.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,58 +1,11 @@
-"""Backend URL Configuration."""
+"""Main URLs Configuration."""
 
 # Django
-from django.urls import path, re_path, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
-# Django Rest Framework
-from rest_framework import routers
-from rest_framework import permissions
-
-# JWT - JSON Web Token
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
-
-# drf-yasg - Yet another Swagger generator
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-
-# CRM
-from crm.views.api import UserApiView as user_views
-from crm.views.api import GroupApiView as group_views
-from crm.views.api import OrganizationApiView as organization_views
-from crm.views.api import ProjectApiView as project_views
-from crm.views.api import ContactApiView as contact_views
-from crm.views.api import MeetingApiView as meeting_views
-
-# Routing
-router = routers.DefaultRouter()
-router.register(r'users', user_views.UserViewSet, basename='user')
-router.register(r'groups', group_views.GroupViewSet, basename='group')
-router.register(r'organizations',
-                organization_views.OrganizationViewSet, basename='organization')
-router.register(r'projects',
-                project_views.ProjectViewSet, basename='project')
-router.register(r'contacts',
-                contact_views.ContactViewSet, basename='contact')
-router.register(r'meetings',
-                meeting_views.MeetingViewSet, basename='meeting')
-
-# API Conf
-schema_view = get_schema_view(
-    openapi.Info(
-        title="CRM API",
-        default_version='v1',
-        description="API Documentation",
-        terms_of_service="127.0.0.1/#",
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 # Routes
 urlpatterns = [
@@ -60,20 +13,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     # API
-    path('api/v1/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
-    # API Documentation
-    re_path(
-        'swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0),
-        name='schema-json'
-    ),
-    path('swagger/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
-    # JWT - JSON Web Token
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/v1/', include('crm.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
